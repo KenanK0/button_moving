432efc8fdd7fb4fcd4db0b27b160a7fe6e1baa54
KenanK0 <kfkasongo4@gmail.com>
2023-04-06 21:51:54 +0200

    first and only commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const moveIt = (e) => {
+    //to move it inside a container only, we need to put the button inside a container,put the container in a relative postion
+    // give the container an height and width
+    //  get the container width and height
+    // const container = e.target.parentElement;
+    // const availableWidth = container.clientWidth - e.target.offsetWidth;
+    // const availableHeight = container.clientHeight - e.target.offsetHeight;
+
+    // e.target.style.backgroundColor = "purple";
+
+    const availableWidth = window.innerWidth - e.target.offsetWidth;
+    const availableHeight = window.innerHeight - e.target.offsetHeight;
+    e.target.style.position = "absolute";
+    e.target.style.top = Math.floor(Math.random() * availableHeight) + "px";
+    e.target.style.left = Math.floor(Math.random() * availableWidth) + "px";
+
+    // e.target.style.backgroundColor = "purple";
+    e.target.style.position = "absolute";
+    e.target.style.top = Math.floor(Math.random() * availableHeight) + "px";
+    e.target.style.left = Math.floor(Math.random() * availableWidth) + "px";
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div id="container"> */}
+        <button onMouseOver={moveIt}>Kenan K</button>
+      {/* </div> */}
     </div>
   );
 }
